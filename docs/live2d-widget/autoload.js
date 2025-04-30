184fc71d667f4fc4e434269751e6c7eaca02ce26
fox0666 <>
2025-04-30 09:10:26 +0000

    🎉auto update by Gmeek action

--- docs/live2d-widget/autoload.js
+++ docs/live2d-widget/autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://blog.369988.xyz/live2d-widget/";
+const live2d_path = "https://blog.369988.xyz/live2d-widget/autoload.js/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			apiPath: "https://liv2d.xicao.dpdns.org/live2d_api/",
-			//cdnPath: "https://blog.369988.xyz/live2d-widget/live2d_api/",
+			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
