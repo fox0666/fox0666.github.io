2f44f6e48ad00743e2a8b3e0a423e5d493973b09
Fox <209303911+fox0666@users.noreply.github.com>
2025-04-30 18:22:46 +0800

    Update autoload.js

--- static/live2d-widget/autoload.js
+++ static/live2d-widget/autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://liv2dapi.369988.xyz/live2d_api/",
+			apiPath: "https://liv2d.xicao.dpdns.org/live2d_api/",
 			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
