93c7e270b96cbdfa931a9683d32c251f96db5440
fox0666 <>
2025-04-30 00:27:49 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [1](https://blog.369988.xyz/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 1632 
-### :alarm_clock: 2025-04-30 08:24:13 
+### :alarm_clock: 2025-04-30 08:27:49 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/live2d-widget/autoload.js
+++ docs/live2d-widget/autoload.js
@@ -34,8 +34,8 @@ if (screen.width >= 768) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://blog.369988.xyz/live2d-widget/live2d_api/",
-			//cdnPath: "https://fastly.jsdelivr.net/gh/fox0666/live2d_api-T@latest/",
+			//apiPath: "https://blog.369988.xyz/live2d-widget/live2d_api/",
+			cdnPath: "https://blog.369988.xyz/live2d-widget/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
